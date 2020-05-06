6ad2e86a8d4d52f38d37918bbfca0b8db8daa4cb
Wade <wade.gushikuma@gmail.com>
2020-05-05 23:17:26 -0700

    thunder finishd

---
+++ Shooter/Assets/Thunder/RotatorRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MirzaBeig.ParticleSystems;
+
+public class RotatorRandomizer : MonoBehaviour
+{
+	[SerializeField]
+	private float maxRot;
+
+	public void ChangeRotation()
+	{
+		Rotator rot = GetComponent<Rotator>();
+		rot.worldRotationSpeed = new Vector3(Random.Range(-maxRot, maxRot), Random.Range(-maxRot, maxRot), Random.Range(-maxRot, maxRot));
+	}
+}
---
+++ Shooter/Assets/Thunder/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject prefab;
+
+	[SerializeField]
+	private Transform parent;
+
+	public float maxSize, minSize;
+	public float maxDuration;
+
+	private List<GameObject> spawnedObjs;
+	
+	private void Start()
+	{
+		spawnedObjs = new List<GameObject>();
+	}
+
+	public void TriggerAction()
+	{
+		GameObject obj = Instantiate(prefab, transform);
+		obj.transform.position = transform.position;
+		float size = Random.Range(minSize,maxSize);
+		obj.transform.localScale = Vector3.one * size;
+		spawnedObjs.Add(obj);
+	}
+
+	public void Despawn()
+	{
+		GameObject lastObj = spawnedObjs[0];
+		lastObj.GetComponent<ActionHandler>().TriggerEvent.Invoke();
+		lastObj.transform.SetParent(parent);
+		spawnedObjs.Remove(lastObj);
+
+		StartCoroutine(RemoveObj(lastObj, Random.Range(1, maxDuration)));
+	}
+
+	IEnumerator RemoveObj(GameObject objToDestroy, float secs)
+	{
+		float count = 0;
+		float startScale = objToDestroy.transform.localScale.x;
+		while (count < secs)
+		{
+			float scale = Mathf.Lerp(startScale, 0, count / secs);
+			//Debug.Log(scale);
+			objToDestroy.transform.localScale = Vector3.one * scale;
+			count += Time.deltaTime;
+			yield return null;
+		}
+		Destroy(objToDestroy);
+		yield return 0;
+	}
+}
